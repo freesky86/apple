3c45aa854f6cb525bc0e96035c6a7a00e988ee37
maxzhang <maxzhang@freesky_zh@hotmail.com>
2019-11-05 19:07:33 +0800

    add java classes.

---
+++ src/main/java/com/freesky/entity/Person.java
@@ -0,0 +1,15 @@
+package com.freesky.entity;
+
+public class Person {
+
+	private String name = "Max Zhang";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
